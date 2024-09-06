e98b311148f6fb86ae3e410e2ff25e7f66c4c19d
BarbozahLuis <luishbarboza0610@gmail.com>
2024-09-05 21:47:52 -0300

    05/09

--- ProjetosNodeJS/ProjetoToDoList/todo-app-jwt/models/User.js
+++ ProjetosNodeJS/ProjetoToDoList/todo-app-jwt/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -14,7 +14,27 @@ const UserSchema = new mongoose.Schema({
   });
 
 
+// Hash a senha antes de salvar
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+
+
+
+// Método para comparar senhas
+UserSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+
   const User = mongoose.models.User || mongoose.model('User', UserSchema);
  
   export default User;
- 
